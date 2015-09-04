ca5fa2e0348f42ae5410fd59eb5c603b4950dc0c
Daniel Glunz <dannyglunz@gmail.com>
2015-09-03 22:07:16 -0600

    cleanup yikyak and remove scratch file

--- yikyakov.rb
+++ yikyakov.rb
@@ -1,6 +1,19 @@
 require 'marky_markov'
-markov = MarkyMarkov::TemporaryDictionary.new
-file_pattern = File.expand_path '../yaks.txt', __FILE__
 
-markov.parse_file file_pattern
-puts markov.generate_n_words 20
+class YikYakov 
+  def initialize
+    @dict = generate_dictionary
+  end
+
+  def generate_post
+    puts @dict.generate_n_words 20
+  end
+
+  private
+
+  def generate_dictionary
+    markov = MarkyMarkov::TemporaryDictionary.new
+    markov.parse_file File.expand_path '../yaks.txt', __FILE__
+    markov
+  end
+end
